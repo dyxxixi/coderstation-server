0d91816156fb7c3e910032b9048ca35cec983cb9
d-yx <352104389@qq.com>
2023-09-09 10:51:32 +0800

    feat:finish project

--- app.js
+++ app.js
@@ -1,41 +1,69 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const session = require("express-session");
+const { ServiceError, UnknownError } = require("./utils/errors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// 默认读取项目根目录下的 .env 环境变量文件
+require("dotenv").config();
+// 进行数据库初始化
+require("./db/init");
 
-var app = express();
+// 引入路由
+const bookRouter = require("./routes/book");
+const issueRouter = require("./routes/issue");
+const adminRouter = require("./routes/admin");
+const captchaRouter = require("./routes/captcha");
+const userRouter = require("./routes/user");
+const typeRouter = require("./routes/type");
+const interviewRouter = require("./routes/interview");
+const commentRouter = require("./routes/comment");
+const uploadRouter = require("./routes/upload");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// 创建服务器实例
+const app = express();
 
-app.use(logger('dev'));
+// 使用 session 中间件
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// 使用路由中间件
+app.use("/api/book", bookRouter);
+app.use("/api/issue", issueRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/user", userRouter);
+app.use("/api/type", typeRouter);
+app.use("/api/comment", commentRouter);
+app.use("/api/interview", interviewRouter);
+app.use("/api/upload", uploadRouter);
+app.use("/res/captcha", captchaRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// 错误处理，一旦发生了错误，就会到这里来
+app.use(function (err, req, res, next) {
+  if (err instanceof ServiceError) {
+    res.send(err.toResponseJSON());
+  } else {
+    res.send(new UnknownError().toResponseJSON());
+    throw err;
+  }
 });
 
 module.exports = app;

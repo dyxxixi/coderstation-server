ee66dc36023b5be294488e3f525ba746764b3bec
d-yx <352104389@qq.com>
2023-10-05 13:54:50 +0800

    chore:give up vercel & mongodb atlas

--- db/connect.js
+++ db/connect.js
@@ -5,10 +5,11 @@
 const mongoose = require("mongoose");
 
 // 定义链接数据库字符串
-// const dbURL = "mongodb://" + process.env.DB_HOST + "/" + process.env.DB_NAME;
+const dbURL = "mongodb://" + process.env.DB_HOST + "/" + process.env.DB_NAME;  // 本地数据库
+// const dbURL = process.env.DB_URL  // 外部数据库 mongodb atlas
 
 // 连接
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // 监听
 mongoose.connection.on("connected", function () {

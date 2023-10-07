3696e42157407c72e1770f7425d34a666cf1389f
d-yx <352104389@qq.com>
2023-10-07 17:22:20 +0800

    chore:update db link

--- db/connect.js
+++ db/connect.js
@@ -5,8 +5,8 @@
 const mongoose = require("mongoose");
 
 // 定义链接数据库字符串
-const dbURL = "mongodb://" + process.env.DB_HOST + "/" + process.env.DB_NAME;  // 本地数据库
-// const dbURL = process.env.DB_URL  // 外部数据库 mongodb atlas
+// const dbURL = "mongodb://" + process.env.DB_HOST + "/" + process.env.DB_NAME;  // 本地数据库
+const dbURL = process.env.DB_URL  // 外部数据库 mongodb atlas
 
 // 连接
 mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
